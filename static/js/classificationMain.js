379ed796a0108ab1c360dad26d7b6576080e589c
mandargogate@outlook.com <mandargogate@outlook.com>
2020-02-13 00:23:38 -0500

    changed violinePlot.js

--- static/js/classificationMain.js
+++ static/js/classificationMain.js
@@ -3,12 +3,11 @@ let accidentData;
 
 d3.csv("\\US_Accidents_Dec19\\US_Accidents_2018.csv").then(function(myData, err) {
     accidentData = myData;  
-    renderViolinePlot(myData, "violinePlot", 'TMC')
+    renderViolinePlot2(myData, "violinePlot", "Severity", 'TMC')
 })//end of d3.csv
 
 
-selectFilters.on("change", function(d){        
-    console.log("hiiiiiii")    
+selectFilters.on("change", function(d){             
     let colName = selectFilters.property("value");
-    renderViolinePlot(accidentData, "violinePlot", colName)    
+    renderViolinePlot2(accidentData, "violinePlot", "Severity", colName)    
 })//end of cmbLocation
--- static/js/violinePlot.js
+++ static/js/violinePlot.js
@@ -1,5 +1,53 @@
 // Plotly.d3.json("https://usacity-accidents-population.herokuapp.com/allcities", function(err, rows){
 
+function renderViolinePlot2(myData, id, col1, col2){
+
+  function unpack(myData, key) {
+    return myData.map(function(myData) { return myData[key]; });
+  }
+
+var data = [{
+  type: 'violin',
+  x: unpack(myData, col1),
+  y: unpack(myData, col2),
+  points: 'none',
+  box: {
+    visible: true
+  },
+  boxpoints: false,
+  line: {
+    color: 'black'
+  },
+  fillcolor: '#8dd3c7',
+  opacity: 0.6,
+  meanline: {
+    visible: true
+  },
+  transforms: [{
+    type: 'groupby',
+  groups: unpack(myData, col1),
+  styles: [
+   {target: '1', value: {line: {color: 'blue'}}},
+   {target: '2', value: {line: {color: 'orange'}}},
+   {target: '3', value: {line: {color: 'green'}}},
+   {target: '4', value: {line: {color: 'red'}}}
+  ]
+ }]
+  // x0: column
+}]
+
+var layout = {
+    width: 1080,
+    height: 450,
+    title: "",
+    yaxis: {
+        zeroline: false
+    }
+}
+
+  Plotly.newPlot(id, data, layout);
+}
+
 
 function renderViolinePlot(myData, id, column){
 
